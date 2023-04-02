3eb23539fcff88262df2dcfea122968ef7f15bd4
Matiaspv <matiasrpv1@gmail.com>
2023-04-01 23:54:11 -0400

    Se crea metodo POST para crear y modifica GET

--- ProyectoPersonas/api/urls.py
+++ ProyectoPersonas/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import PersonaView
 
 urlpatterns=[
-    path('personas/',PersonaView.as_view(), name='personas_list')
+    path('personas/',PersonaView.as_view(), name='personas_list'),
+    path('personas/<int:id>',PersonaView.as_view(), name='personas_process')
 ]
--- ProyectoPersonas/api/views.py
+++ ProyectoPersonas/api/views.py
@@ -1,22 +1,43 @@
 from django.http import JsonResponse
 from django.views import View
 from .models import Persona
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 
 
 class PersonaView(View):
     
-    def get(self, request):
-        personas= list(Persona.objects.values())
-        if len(personas)>0:
-            datos={'message':"Success",'personas':personas}
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request,id=0):
+        if(id>0):
+            personas= list(Persona.objects.filter(id=id).values())
+            if len(personas) > 0:
+                persona = personas[0]
+                datos={'message':"Success",'personas':persona}
+            else:
+                datos={'message':"Persona not found..."}
+            return JsonResponse(datos)
         else:
-            datos={'message':"Personas not found..."}
-        return JsonResponse(datos)
+            personas= list(Persona.objects.values())
+            if len(personas)>0:
+                datos={'message':"Success",'personas':personas}
+            else:
+                datos={'message':"Personas not found..."}
+            return JsonResponse(datos)
         
     def post(self, request):
-        pass
+        ## print(request.body)
+        jd= json.loads(request.body)
+        ##print(jd)
+        Persona.objects.create(name=jd['name'],lastname=jd['lastname'],age=jd['age'])
+        datos={'message':"Success"}
+        return JsonResponse(datos)
     def put(self, request):
         pass
     def delete(self, request):
