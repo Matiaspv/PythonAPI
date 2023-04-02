ba5e3c6d6922028c20148e7cb269229b59b3872f
Matiaspv <matiasrpv1@gmail.com>
2023-04-01 23:15:35 -0400

    Se conecta a base de datos mysql y se crea modelo de personas

--- ProyectoPersonas/ProyectoPersonas/settings.py
+++ ProyectoPersonas/ProyectoPersonas/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,16 @@ WSGI_APPLICATION = 'ProyectoPersonas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'zeobjerg1234',
+        'NAME': 'django_api',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+
     }
 }
 
@@ -103,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
 TIME_ZONE = 'UTC'
 
--- ProyectoPersonas/api/models.py
+++ ProyectoPersonas/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Persona(models.Model):
+    name=models.CharField(max_length=50)
+    lastname=models.CharField(max_length=50)
+    age=models.PositiveIntegerField()
