987e024574a46d90b71ae38986277b3c4f4c6192
Matiaspv <matiasrpv1@gmail.com>
2023-04-02 00:11:20 -0400

    !modificar pass en db en settings.py para usar!

--- ProyectoPersonas/ProyectoPersonas/settings.py
+++ ProyectoPersonas/ProyectoPersonas/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
         'USER': 'root',
-        'PASSWORD': 'zeobjerg1234',
+        'PASSWORD': '',
         'NAME': 'django_api',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
